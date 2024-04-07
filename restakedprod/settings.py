9433defa69667787a3077161fd4484020c63cebd
0xhiro <0xhiro@tutamail.com>
2024-04-06 22:33:30 -0400

    allowed host

--- restakedprod/settings.py
+++ restakedprod/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['restaked-staging-a672bdd3da7f.herokuapp.com']
 
 
 # Application definition
